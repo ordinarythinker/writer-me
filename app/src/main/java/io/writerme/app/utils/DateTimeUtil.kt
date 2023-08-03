53cbfc99c1fed0ddf1bfaf98598e15f9f6b28cce
Andrii Nexter <88105032+mr-nexter@users.noreply.github.com>
2023-08-04 02:18:45 +0300

    DateTimeUtil: today, yesterday and tomorrow added

--- app/src/main/java/io/writerme/app/utils/DateTimeUtil.kt
+++ app/src/main/java/io/writerme/app/utils/DateTimeUtil.kt
@@ -7,11 +7,31 @@ import java.util.Date
 @Composable
 fun toDateDescription(date: Date): String {
     var result = "Today at 2:40 PM"
-    val current = Calendar.getInstance()
+    val now = Calendar.getInstance()
     val given = Calendar.getInstance()
     given.time = date
 
-//    if (current.get(Calendar.))
+    val yesterday = Calendar.getInstance()
+    yesterday.add(Calendar.DAY_OF_YEAR, -1)
+
+    val tomorrow = Calendar.getInstance()
+    tomorrow.add(Calendar.DAY_OF_YEAR, 1)
+
+    if (given.get(Calendar.YEAR) == now.get(Calendar.YEAR) &&
+        given.get(Calendar.DAY_OF_YEAR) == now.get(Calendar.DAY_OF_YEAR)
+    ) {
+        // today
+    } else if (given.get(Calendar.YEAR) == yesterday[Calendar.YEAR] &&
+        given.get(Calendar.DAY_OF_YEAR) == yesterday[Calendar.DAY_OF_YEAR]
+    ) {
+        println("The date was yesterday!")
+    } else if (given.get(Calendar.YEAR) == tomorrow[Calendar.YEAR] &&
+        given.get(Calendar.DAY_OF_YEAR) == tomorrow[Calendar.DAY_OF_YEAR]
+    ) {
+        println("The date is tomorrow!")
+    } else {
+        println("The date is not yesterday or tomorrow!")
+    }
 
 
     return result
