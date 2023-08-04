b6975ef5f38004475b9d7478734aa0f352bba2a1
Andrii Nexter <88105032+mr-nexter@users.noreply.github.com>
2023-08-05 01:50:09 +0300

    NoteScreen: Scaffold is added

--- app/src/main/java/io/writerme/app/ui/screen/NoteScreen.kt
+++ app/src/main/java/io/writerme/app/ui/screen/NoteScreen.kt
@@ -1,8 +1,89 @@
 package io.writerme.app.ui.screen
 
+import android.annotation.SuppressLint
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.Icon
+import androidx.compose.material.IconButton
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Scaffold
+import androidx.compose.material.Text
+import androidx.compose.material.TopAppBar
+import androidx.compose.material.rememberScaffoldState
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import io.writerme.app.R
+import io.writerme.app.ui.theme.WriterMeTheme
+import io.writerme.app.ui.theme.light
 
+@SuppressLint("UnusedMaterialScaffoldPaddingParameter")
 @Composable
 fun NoteScreen() {
+    val scaffoldState = rememberScaffoldState()
 
+    Box(modifier = Modifier.fillMaxSize()) {
+        Image(
+            painter = painterResource(id = R.drawable.background_main),
+            contentScale = ContentScale.Crop,
+            contentDescription = stringResource(id = R.string.background_image),
+            modifier = Modifier.fillMaxSize()
+        )
+
+        Scaffold(
+            scaffoldState = scaffoldState,
+            backgroundColor = Color.Transparent,
+            topBar = {
+                TopAppBar(
+                    backgroundColor = Color.Transparent,
+                    elevation = 0.dp,
+                    title = {
+                        Text(
+                            text = stringResource(id = R.string.edit),
+                            style = MaterialTheme.typography.h2,
+                            color = MaterialTheme.colors.light
+                        )
+                    },
+                    navigationIcon = {
+                        IconButton(onClick = {  }) {
+                            Icon(
+                                painter = painterResource(id = R.drawable.ic_back),
+                                contentDescription = stringResource(id = R.string.back_button),
+                                tint = MaterialTheme.colors.light
+                            )
+                        }
+                    },
+                    actions = {
+                        IconButton(onClick = {  }) {
+                            Icon(
+                                painter = painterResource(id = R.drawable.ic_more),
+                                contentDescription = stringResource(id = R.string.more),
+                                tint = MaterialTheme.colors.light
+                            )
+                        }
+                    }
+                )
+            },
+            content = {
+
+            },
+            bottomBar = {
+
+            }
+        )
+    }
+}
+
+@Preview
+@Composable
+fun NoteScreenPreview() {
+    WriterMeTheme {
+        NoteScreen()
+    }
 }
---
+++ app/src/main/java/io/writerme/app/ui/screen/TasksScreen.kt
@@ -0,0 +1,19 @@
+package io.writerme.app.ui.screen
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import io.writerme.app.ui.theme.WriterMeTheme
+
+@Composable
+fun TaskScreen() {
+    // TODO
+}
+
+@Preview(showBackground = true, showSystemUi = true)
+@Composable
+fun TaskScreenPreview() {
+
+    WriterMeTheme {
+        TaskScreen()
+    }
+}
