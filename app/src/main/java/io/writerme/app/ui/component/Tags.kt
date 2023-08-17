9d60f3aad2c7221fda3cb72287927bf49ce6f572
Andrii Nexter <88105032+mr-nexter@users.noreply.github.com>
2023-08-18 00:53:54 +0300

    TagsBar: template is added

---
+++ app/src/main/java/io/writerme/app/ui/component/Tags.kt
@@ -0,0 +1,18 @@
+package io.writerme.app.ui.component
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import io.writerme.app.ui.theme.WriterMeTheme
+
+@Composable
+fun TagsBar() {
+
+}
+
+@Preview(showBackground = true)
+@Composable
+fun TagsBarPreview() {
+    WriterMeTheme {
+        TagsBar()
+    }
+}
