6d8b8b83a4b9d11a3677231f436857b7735b0c77
Andrii Nexter <88105032+mr-nexter@users.noreply.github.com>
2023-08-18 02:02:36 +0300

    TagsBar: Main UI components were added

--- app/src/main/java/io/writerme/app/ui/component/Tags.kt
+++ app/src/main/java/io/writerme/app/ui/component/Tags.kt
@@ -1,44 +1,71 @@
 package io.writerme.app.ui.component
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.ExperimentalLayoutApi
 import androidx.compose.foundation.layout.FlowRow
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.BasicTextField
+import androidx.compose.material.Chip
 import androidx.compose.material.Divider
+import androidx.compose.material.ExperimentalMaterialApi
+import androidx.compose.material.Icon
 import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Close
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.input.key.Key
 import androidx.compose.ui.input.key.KeyEventType
 import androidx.compose.ui.input.key.key
 import androidx.compose.ui.input.key.onKeyEvent
 import androidx.compose.ui.input.key.type
+import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import io.writerme.app.R
 import io.writerme.app.ui.theme.WriterMeTheme
+import io.writerme.app.ui.theme.light
 import io.writerme.app.ui.theme.strokeLight
 
-@OptIn(ExperimentalLayoutApi::class, ExperimentalComposeUiApi::class)
+@OptIn(
+    ExperimentalLayoutApi::class,
+    ExperimentalComposeUiApi::class,
+    ExperimentalMaterialApi::class
+)
 @Composable
 fun TagsBar(
     tags: List<String>,
     addNewTag: (String) -> Unit,
+    deleteTag: (String) -> Unit,
     modifier: Modifier = Modifier
 ) {
     var text by remember {
         mutableStateOf("")
     }
 
+    val padding = dimensionResource(id = R.dimen.screen_padding)
+
     Column(
-        modifier = modifier.fillMaxWidth()
+        modifier = modifier
+            .fillMaxWidth()
+            .background(Color.Gray)
     ) {
         Divider(
             modifier = Modifier.fillMaxWidth(),
@@ -48,28 +75,68 @@ fun TagsBar(
 
         Spacer(modifier = Modifier.height(8.dp))
 
-        FlowRow {
-            for (tag in tags) {
-                // TODO: here will be chips
-            }
-
-
-            // TODO: add here the spacing modifier
-            BasicTextField(
-                modifier = Modifier.onKeyEvent {
-                    if (it.type == KeyEventType.KeyDown || it.key == Key.Enter) {
-                        addNewTag(text)
-                        true
-                    } else false
-                },
-                value = text,
-                onValueChange = {
-                    text = it
-                },
-                decorationBox = {
-                    // TODO: here will be the placeholder
-                }
+        Row {
+            Text(
+                text = stringResource(id = R.string.tags),
+                style = MaterialTheme.typography.body1,
+                color = MaterialTheme.colors.light,
+                modifier = Modifier.padding(end = padding)
             )
+
+            FlowRow(
+                modifier = Modifier.weight(1f)
+            ) {
+                for (tag in tags) {
+                    Chip(
+                        onClick = {  },
+                        shape = RoundedCornerShape(padding),
+                        enabled = true,
+                        leadingIcon = {
+                            Icon(
+                                imageVector = Icons.Default.Close,
+                                contentDescription = null,
+                                tint = MaterialTheme.colors.light,
+                                modifier = Modifier.clickable { deleteTag(tag) }
+                            )
+                        }
+                    ) {
+                        Text(
+                            text = "#$tag",
+                            style = MaterialTheme.typography.body1,
+                            color = MaterialTheme.colors.light
+                        )
+                    }
+
+                    Spacer(modifier = Modifier.width(padding))
+                }
+
+
+                // TODO: add here the spacing modifier
+                BasicTextField(
+                    modifier = Modifier.onKeyEvent {
+                        if (it.type == KeyEventType.KeyDown || it.key == Key.Enter) {
+                            addNewTag(text)
+                            true
+                        } else false
+                    },
+                    value = text,
+                    onValueChange = {
+                        text = it
+                    },
+                    decorationBox = { innerTextField ->
+                        Row {
+                            if (text.isEmpty()) {
+                                Text(
+                                    text = stringResource(id = R.string.add_hashtags),
+                                    style = MaterialTheme.typography.body1,
+                                    color = MaterialTheme.colors.light
+                                )
+                            }
+                            innerTextField()
+                        }
+                    }
+                )
+            }
         }
     }
 }
@@ -78,6 +145,6 @@ fun TagsBar(
 @Composable
 fun TagsBarPreview() {
     WriterMeTheme {
-        TagsBar(listOf(), {})
+        TagsBar(listOf("traveling", "unstoppable", "dreamer"), {}, {})
     }
 }
