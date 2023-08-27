2d8b5bf35e9f5e04a2c4174d205237de57ce2c92
Andrii Nexter <88105032+mr-nexter@users.noreply.github.com>
2023-08-27 22:45:04 +0300

    HomeScreen: main template is added

--- app/src/main/java/io/writerme/app/ui/screen/HomeScreen.kt
+++ app/src/main/java/io/writerme/app/ui/screen/HomeScreen.kt
@@ -1,8 +1,27 @@
 package io.writerme.app.ui.screen
 
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import io.writerme.app.ui.state.MainState
+import io.writerme.app.ui.theme.WriterMeTheme
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
 
 @Composable
-fun HomeScreen() {
+fun HomeScreen(stateFlow: StateFlow<MainState>) {
+    val state = stateFlow.collectAsStateWithLifecycle()
 
+}
+
+@Preview
+@Composable
+fun HomeScreenPreview() {
+    val main = MainState()
+
+    val flow = MutableStateFlow(main)
+
+    WriterMeTheme {
+        HomeScreen(stateFlow = flow)
+    }
 }
