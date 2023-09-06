ccdc0d158567e19e5f62c9d42f319583d5d5fda6
Andrii Nexter <88105032+mr-nexter@users.noreply.github.com>
2023-09-06 20:24:06 +0300

    NoteRepository, NoteState and NoteViewModel have been updated

--- app/src/main/java/io/writerme/app/ui/state/NoteState.kt
+++ app/src/main/java/io/writerme/app/ui/state/NoteState.kt
@@ -2,9 +2,13 @@ package io.writerme.app.ui.state
 
 import io.writerme.app.data.model.Note
 
-class NoteState(
+data class NoteState(
     val note: Note,
     val isHastTagBarVisible: Boolean = false,
     val isHistoryMode: Boolean = false,
     val tags: List<String>
-)
+) {
+    companion object {
+        fun empty(): NoteState = NoteState(Note(), tags = listOf())
+    }
+}
--- app/src/main/java/io/writerme/app/viewmodel/NoteViewModel.kt
+++ app/src/main/java/io/writerme/app/viewmodel/NoteViewModel.kt
@@ -3,20 +3,54 @@ package io.writerme.app.viewmodel
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
+import io.realm.kotlin.notifications.ResultsChange
 import io.writerme.app.data.model.Component
 import io.writerme.app.data.model.History
 import io.writerme.app.data.repository.NoteRepository
+import io.writerme.app.ui.state.NoteState
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.FlowPreview
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.debounce
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.mapLatest
+import kotlinx.coroutines.flow.onEach
+import kotlinx.coroutines.flow.stateIn
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
+@OptIn(ExperimentalCoroutinesApi::class, FlowPreview::class)
 @HiltViewModel
 class NoteViewModel @Inject constructor(): ViewModel() {
 
     private val noteRepository: NoteRepository = NoteRepository()
     private val changes : HashMap<Long, Int> = hashMapOf()
 
+    private val pendingUpdates = mutableMapOf<Long, Component>()
+    private val saveFlow = MutableSharedFlow<Component>()
+
+    private lateinit var _componentsSource: Flow<ResultsChange<Component>>
+
+    private val _noteState: MutableStateFlow<NoteState> = MutableStateFlow(NoteState.empty())
+    val noteState: StateFlow<NoteState> = _noteState
+
     init {
         addCloseable(noteRepository)
+
+        viewModelScope.launch {
+            _componentsSource.mapLatest {
+                //_noteState.emit(_noteState.value.copy(components = it.list))
+            }.stateIn(viewModelScope)
+        }
+
+        saveFlow.debounce(300)
+            .onEach { component ->
+                pendingUpdates.remove(component.id)
+                noteRepository.saveComponent(component)
+            }.launchIn(viewModelScope)
     }
 
     fun modifyHistory(history: History, component: Component) {
@@ -33,7 +67,9 @@ class NoteViewModel @Inject constructor(): ViewModel() {
     }
 
     fun saveChanges() {
+        viewModelScope.launch {
 
+        }
     }
 
     override fun onCleared() {
