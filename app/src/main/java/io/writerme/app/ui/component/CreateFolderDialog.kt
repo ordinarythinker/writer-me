f0e8e8b2ef0831fc895eef501e5dc66b03a9786e
Andrii Nexter <88105032+mr-nexter@users.noreply.github.com>
2023-09-08 21:36:03 +0300

    TitledDialog is added. Moved CreateFolderDialogBody to TitleDialog call

--- app/src/main/java/io/writerme/app/ui/component/CreateFolderDialog.kt
+++ app/src/main/java/io/writerme/app/ui/component/CreateFolderDialog.kt
@@ -3,7 +3,6 @@ package io.writerme.app.ui.component
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -13,7 +12,6 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.BasicTextField
-import androidx.compose.material.Card
 import androidx.compose.material.Divider
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
@@ -24,121 +22,93 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.blur
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.window.Dialog
 import io.writerme.app.R
 import io.writerme.app.ui.theme.WriterMeTheme
-import io.writerme.app.ui.theme.dialogBackground
 import io.writerme.app.ui.theme.fieldDark
 import io.writerme.app.ui.theme.light
 import io.writerme.app.ui.theme.strokeLight
 
 @Composable
-fun CreateFolderDialog(
+fun CreateFolderDialogBody(
     createFolder: (name: String) -> Unit,
     onDismiss: () -> Unit,
-    modifier: Modifier = Modifier,
 ) {
-    val shape = RoundedCornerShape(dimensionResource(id = R.dimen.big_radius))
     val padding = dimensionResource(id = R.dimen.screen_padding)
 
     var name by remember {
         mutableStateOf("")
     }
 
-    Dialog(
-        onDismissRequest = onDismiss
+    Column(
+        modifier = Modifier.padding(padding)
     ) {
-        Card(
-            shape = shape,
-            modifier = modifier
-                .wrapContentHeight()
-                .shadow(dimensionResource(id = R.dimen.shadow), shape)
-        ) {
-            Box(modifier = Modifier
-                .fillMaxWidth()
-                .wrapContentHeight()) {
-                Box(
-                    modifier = Modifier
-                        .matchParentSize()
-                        .background(MaterialTheme.colors.dialogBackground)
-                        .blur(dimensionResource(id = R.dimen.blur_radius))
-                )
+        Text(
+            modifier = Modifier.padding(0.dp, 8.dp, 0.dp, padding),
+            text = stringResource(id = R.string.create_folder),
+            style = MaterialTheme.typography.h4,
+            color = MaterialTheme.colors.light
+        )
 
-                Column(
-                    modifier = Modifier.padding(padding)
-                ) {
-                    Text(
-                        modifier = Modifier.padding(0.dp, 8.dp, 0.dp, padding),
-                        text = stringResource(id = R.string.create_folder),
-                        style = MaterialTheme.typography.h4,
-                        color = MaterialTheme.colors.light
-                    )
+        Divider(
+            thickness = 1.dp,
+            color = MaterialTheme.colors.strokeLight
+        )
 
-                    Divider(
-                        thickness = 1.dp,
-                        color = MaterialTheme.colors.strokeLight
-                    )
+        Spacer(modifier = Modifier.height(padding))
 
-                    Spacer(modifier = Modifier.height(padding))
+        val fieldShape = RoundedCornerShape(dimensionResource(id = R.dimen.big_radius))
 
-                    val fieldShape = RoundedCornerShape(dimensionResource(id = R.dimen.big_radius))
+        val backgroundModifier = Modifier
+            .clip(fieldShape)
+            .border(
+                dimensionResource(id = R.dimen.field_border_width),
+                MaterialTheme.colors.strokeLight,
+                fieldShape
+            )
+            .background(MaterialTheme.colors.fieldDark)
+            .padding(padding, 0.dp)
+            .height(40.dp)
 
-                    val backgroundModifier = Modifier
-                        .clip(fieldShape)
-                        .border(
-                            dimensionResource(id = R.dimen.field_border_width),
-                            MaterialTheme.colors.strokeLight,
-                            fieldShape
+        BasicTextField(
+            value = name,
+            maxLines = 1,
+            onValueChange = { name = it },
+            modifier = backgroundModifier.fillMaxWidth(),
+            singleLine = true,
+            textStyle = MaterialTheme.typography.body1.copy(color = MaterialTheme.colors.light),
+            decorationBox = { innerTextField ->
+                Row(verticalAlignment = Alignment.CenterVertically) {
+                    if (name.isEmpty()) {
+                        Text(
+                            text = stringResource(id = R.string.folder_name),
+                            style = MaterialTheme.typography.body1,
+                            color = MaterialTheme.colors.light
                         )
-                        .background(MaterialTheme.colors.fieldDark)
-                        .padding(padding, 0.dp)
-                        .height(40.dp)
-
-                    BasicTextField(
-                        value = name,
-                        maxLines = 1,
-                        onValueChange = { name = it },
-                        modifier = backgroundModifier.fillMaxWidth(),
-                        singleLine = true,
-                        textStyle = MaterialTheme.typography.body1.copy(color = MaterialTheme.colors.light),
-                        decorationBox = { innerTextField ->
-                            Row(verticalAlignment = Alignment.CenterVertically) {
-                                if (name.isEmpty()) {
-                                    Text(
-                                        text = stringResource(id = R.string.folder_name),
-                                        style = MaterialTheme.typography.body1,
-                                        color = MaterialTheme.colors.light
-                                    )
-                                }
-                                innerTextField()
-                            }
-                        },
-                    )
-
-                    Spacer(modifier = Modifier.height(padding))
-
-                    Text(
-                        text = stringResource(id = R.string.create),
-                        modifier = backgroundModifier
-                            .wrapContentHeight()
-                            .padding(20.dp, 0.dp)
-                            .align(Alignment.CenterHorizontally)
-                            .clickable {
-                                createFolder(name)
-                            },
-                        color = MaterialTheme.colors.light
-                    )
+                    }
+                    innerTextField()
                 }
+            },
+        )
 
-            }
-        }
+        Spacer(modifier = Modifier.height(padding))
+
+        Text(
+            text = stringResource(id = R.string.create),
+            modifier = backgroundModifier
+                .wrapContentHeight()
+                .padding(20.dp, 0.dp)
+                .align(Alignment.CenterHorizontally)
+                .clickable {
+                    createFolder(name)
+                    onDismiss()
+                },
+            color = MaterialTheme.colors.light
+        )
     }
 }
 
@@ -146,6 +116,6 @@ fun CreateFolderDialog(
 @Composable
 fun CreateFolderDialogPreview() {
     WriterMeTheme {
-        CreateFolderDialog(createFolder = {}, onDismiss = {}, modifier = Modifier)
+        CreateFolderDialogBody(createFolder = {}, onDismiss = {})
     }
 }
---
+++ app/src/main/java/io/writerme/app/ui/component/TitledDialog.kt
@@ -0,0 +1,92 @@
+package io.writerme.app.ui.component
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Card
+import androidx.compose.material.Divider
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.blur
+import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.window.Dialog
+import io.writerme.app.R
+import io.writerme.app.ui.theme.WriterMeTheme
+import io.writerme.app.ui.theme.dialogBackground
+import io.writerme.app.ui.theme.light
+import io.writerme.app.ui.theme.strokeLight
+
+@Composable
+fun TitledDialog(
+    title: String,
+    onDismiss: () -> Unit,
+    modifier: Modifier = Modifier,
+    content: @Composable () -> Unit
+) {
+    val shape = RoundedCornerShape(dimensionResource(id = R.dimen.big_radius))
+
+    val padding = dimensionResource(id = R.dimen.screen_padding)
+
+    Dialog(
+        onDismissRequest = onDismiss
+    ) {
+        Card(
+            shape = shape,
+            modifier = modifier
+                .wrapContentHeight()
+                .shadow(dimensionResource(id = R.dimen.shadow), shape)
+        ) {
+            Box(modifier = Modifier
+                .fillMaxWidth()
+                .wrapContentHeight()) {
+                Box(
+                    modifier = Modifier
+                        .matchParentSize()
+                        .background(MaterialTheme.colors.dialogBackground)
+                        .blur(dimensionResource(id = R.dimen.blur_radius))
+                )
+
+                Column {
+                    Text(
+                        modifier = Modifier.padding(0.dp, 8.dp, 0.dp, padding),
+                        text = stringResource(id = R.string.create_folder),
+                        style = MaterialTheme.typography.h4,
+                        color = MaterialTheme.colors.light
+                    )
+
+                    Divider(
+                        thickness = 1.dp,
+                        color = MaterialTheme.colors.strokeLight
+                    )
+
+                    Spacer(modifier = Modifier.height(padding))
+
+                    content()
+                }
+            }
+        }
+    }
+}
+
+@Preview
+@Composable
+fun TitledDialogPreview() {
+    WriterMeTheme {
+        TitledDialog(
+            "", {},
+            content = {}
+        )
+    }
+}
