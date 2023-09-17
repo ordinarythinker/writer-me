cf51aedd371e9ffbad32829a9d74f2ad6cbcf5b4
Andrii Nexter <88105032+mr-nexter@users.noreply.github.com>
2023-09-17 22:05:39 +0300

    RegistrationScreen: background is added

--- app/src/main/java/io/writerme/app/ui/screen/RegistrationScreen.kt
+++ app/src/main/java/io/writerme/app/ui/screen/RegistrationScreen.kt
@@ -1,12 +1,27 @@
 package io.writerme.app.ui.screen
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import io.writerme.app.R
 import io.writerme.app.ui.theme.WriterMeTheme
 
 @Composable
 fun RegistrationScreen() {
+    Box(modifier = Modifier.fillMaxSize()) {
+        Image(
+            painter = painterResource(id = R.drawable.background_main),
+            contentDescription = "",
+            modifier = Modifier.fillMaxSize(),
+            contentScale = ContentScale.Crop
+        )
 
+    }
 }
 
 @Preview
