a7d1afeeff22851c65d19aa879fb00f2760c7afe
Andrii Nexter <88105032+mr-nexter@users.noreply.github.com>
2023-09-17 22:04:24 +0300

    RegistrationScreen template is added

---
+++ app/src/main/java/io/writerme/app/ui/screen/RegistrationScreen.kt
@@ -0,0 +1,18 @@
+package io.writerme.app.ui.screen
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import io.writerme.app.ui.theme.WriterMeTheme
+
+@Composable
+fun RegistrationScreen() {
+
+}
+
+@Preview
+@Composable
+fun RegistrationScreenPreview() {
+    WriterMeTheme {
+        RegistrationScreen()
+    }
+}
