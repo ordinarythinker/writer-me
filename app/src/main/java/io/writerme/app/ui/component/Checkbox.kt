2c192e6ff07ae86cc0e479a3395baca3d790188b
Andrii Nexter <88105032+mr-nexter@users.noreply.github.com>
2023-10-17 12:53:01 +0300

    Checkbox: bug fixing

--- app/src/main/java/io/writerme/app/ui/component/Checkbox.kt
+++ app/src/main/java/io/writerme/app/ui/component/Checkbox.kt
@@ -6,6 +6,7 @@ import androidx.compose.foundation.clickable
 import androidx.compose.foundation.focusable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.defaultMinSize
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
@@ -79,6 +80,7 @@ fun Checkbox(
                 modifier = Modifier
                     .padding(16.dp, 0.dp, 0.dp, 0.dp)
                     .fillMaxWidth()
+                    .defaultMinSize(minHeight = 48.dp)
                     .focusRequester(requester)
                     .focusable()
                     .onKeyEvent {
